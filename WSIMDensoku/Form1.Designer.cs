22f9986f0d1f25d0563134f45f8a72efb8d03837
Se AKi <seaki@sastudio.jp>
2021-01-31 23:56:46 +0900

    プロジェクト ファイルを追加します。

---
+++ WSIMDensoku/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace WSIMDensoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.comPortLabel = new System.Windows.Forms.Label();
+            this.comPortTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(13, 13);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "スタート(&S)";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(95, 13);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 1;
+            this.stopButton.Text = "停止(&T)";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(13, 43);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.resultTextBox.Size = new System.Drawing.Size(775, 395);
+            this.resultTextBox.TabIndex = 2;
+            this.resultTextBox.WordWrap = false;
+            // 
+            // comPortLabel
+            // 
+            this.comPortLabel.AutoSize = true;
+            this.comPortLabel.Location = new System.Drawing.Point(176, 18);
+            this.comPortLabel.Name = "comPortLabel";
+            this.comPortLabel.Size = new System.Drawing.Size(73, 12);
+            this.comPortLabel.TabIndex = 3;
+            this.comPortLabel.Text = "COMポート(&P)";
+            // 
+            // comPortTextBox
+            // 
+            this.comPortTextBox.Location = new System.Drawing.Point(255, 15);
+            this.comPortTextBox.Name = "comPortTextBox";
+            this.comPortTextBox.Size = new System.Drawing.Size(100, 19);
+            this.comPortTextBox.TabIndex = 4;
+            this.comPortTextBox.Text = "COM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comPortTextBox);
+            this.Controls.Add(this.comPortLabel);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Label comPortLabel;
+        private System.Windows.Forms.TextBox comPortTextBox;
+    }
+}
+
---
+++ WSIMDensoku/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WSIMDensoku
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort _port = null;
+        private bool _stopped = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void startButton_Click(object sender, EventArgs e)
+        {
+            _stopped = false;
+            // TODO: 別スレッドでポートを開き、5秒ごとに AT@K20 と AT@@LVL を送る
+            openPort(comPortTextBox.Text);
+            await Task.Run(async () =>
+            {
+                while (!_stopped)
+                {
+                    sendData("AT@K20");
+                    await Task.Delay(5000);
+                    sendData("AT@@LVL");
+                    await Task.Delay(1000);
+                }
+            });
+            
+        }
+
+        private void openPort(string portName)
+        {
+            _port = new SerialPort
+            {
+                PortName = portName,
+                BaudRate = 9600,
+                Parity = Parity.None,
+                DataBits = 8,
+                StopBits = StopBits.One,
+                DtrEnable = true,
+                RtsEnable = true,
+                NewLine = "\r",
+                Encoding = Encoding.ASCII,
+            };
+            if (_port.IsOpen) { _port.Close(); }
+            _port.DataReceived += _port_DataReceived;
+            _port.Open();
+        }
+
+        private void sendData(string text)
+        {
+            //Invoke((Action)(() => {
+            //    resultTextBox.Text += string.Format("{0:s} >> ", DateTime.Now) + text + Environment.NewLine;
+            //}));
+            _port.WriteLine(text);
+        }
+
+        private void _port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string text = _port.ReadExisting();
+            Invoke((Action)(() => {
+                //resultTextBox.AppendText(string.Format("{0:s} >> {2}{1}{2}", DateTime.Now, text, Environment.NewLine));
+                resultTextBox.AppendText(text);
+                resultTextBox.ScrollToCaret();
+            }));
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            _port.Close();
+            _port.Dispose();
+            _port = null;
+            _stopped = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (null != _port)
+            {
+                _port.Dispose();
+            }
+        }
+    }
+}
